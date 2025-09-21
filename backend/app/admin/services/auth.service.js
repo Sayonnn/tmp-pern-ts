65dde91b3ba4e31a9b1fbaf19d5f98c162191d41
Laud Zion Cascalla <93295452+Sayonnn@users.noreply.github.com>
2025-09-21 18:56:40 +0800

    fix: admin login

--- backend/app/admin/services/auth.service.js
+++ backend/app/admin/services/auth.service.js
@@ -49,7 +49,7 @@ export const registerAdmin = async (
 
 export const loginAdmin = async (username, password) => {
     /** Check Username Exists */
-    const checkUserSql = "SELECT id, email, password, permissions, super_admin, username, role, created_at FROM _admins WHERE username = $1";
+    const checkUserSql = `SELECT id, email, password, permissions, super_admin, username, role, created_at FROM ${config.db.abbr}_admins WHERE username = $1`;
     const existing = await startQuery(checkUserSql, [username]);
   
     if (existing.rows.length === 0) {
